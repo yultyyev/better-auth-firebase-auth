58b48b15e9f790c78f42d095e69dc5481a67c7e2
Slava Yultyyev <yultyyev@gmail.com>
2025-11-13 10:59:33 -0800

    fix: fix import organization in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export {
-	firebaseAuthClientPlugin,
 	extractOobCodeFromUrl,
+	firebaseAuthClientPlugin,
 } from "./firebase-auth-client-plugin";
 export { firebaseAuthPlugin } from "./firebase-auth-plugin";
 export type * from "./types";
